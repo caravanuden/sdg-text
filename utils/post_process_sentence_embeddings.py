5cd8bc4a18af35a87ebb6e1e494dfbae1ca2584a
Cara Van Uden <cara.vanuden@gmail.com>
2021-11-30 17:10:57 -0800

    cleanup postprocess code

--- utils/post_process_sentence_embeddings.py
+++ utils/post_process_sentence_embeddings.py
@@ -1,24 +1,22 @@
 import numpy as np
 import pandas as pd
 import os
-from utils.constants import (
-    INTERMEDIATE_DATA_DIR,
-    OUTPUT_DATA_DIR,
+from constants import (
+    INTERMEDIATE_SENTENCE_DATA_DIR,
+    OUTPUT_SENTENCE_DATA_DIR,
     LABEL_METADATA_PATH,
     COUNTRIES,
     TARGETS,
 )
 
 
-def postprocess_sentence_embeddings(input_dir, output_dir, key="DHSID_EA"):
+def post_process_sentence_embeddings(input_dir, output_dir, key="DHSID_EA"):
     ids = pd.read_csv(os.path.join(input_dir, "metadata.csv"))
     metadata = pd.read_csv(LABEL_METADATA_PATH)
     metadata = ids.merge(metadata, on=key, how="left")[[key, target]]
 
     locations = metadata.DHSID_EA.unique()
-    intermediate_embeddings = np.load(
-        os.path.join(input_data_dir, country, target, "embeddings.npy")
-    )
+    intermediate_embeddings = np.load(os.path.join(input_dir, "embeddings.npy"))
 
     consolidated_metadata = metadata.drop_duplicates(subset=[key])
     consolidated_embeddings = np.zeros(
@@ -34,7 +32,45 @@ def postprocess_sentence_embeddings(input_dir, output_dir, key="DHSID_EA"):
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
 
-    consolidated_metadata.to_csv(os.path.join(output_dir, "metadata.csv"))
+    consolidated_metadata.to_csv(os.path.join(output_dir, "metadata.csv"), index=False)
+
+    np.save(
+        os.path.join(output_dir, "embeddings.npy"), consolidated_embeddings,
+    )
+
+
+def post_process_sentence_embeddings_all(input_dir, output_dir, key="DHSID_EA"):
+    ids = [
+        pd.read_csv(os.path.join(input_dir, target, "metadata.csv"))
+        for target in TARGETS
+    ]
+    ids = pd.concat(ids)[key].to_frame()
+
+    metadata = pd.read_csv(LABEL_METADATA_PATH)
+    metadata = ids.merge(metadata, on=key, how="left")[[key] + TARGETS]
+
+    locations = metadata.DHSID_EA.unique()
+    consolidated_metadata = metadata.drop_duplicates(subset=[key])
+
+    intermediate_embeddings = [
+        np.load(os.path.join(input_dir, target, "embeddings.npy")) for target in TARGETS
+    ]
+    intermediate_embeddings = np.concatenate(intermediate_embeddings, axis=0)
+    consolidated_embeddings = np.zeros(
+        (len(locations), intermediate_embeddings.shape[1])
+    )
+    for i, location in enumerate(locations):
+        j = metadata[key] == location
+        consolidated_embeddings[i, :] = np.mean(
+            intermediate_embeddings[metadata[key] == location], axis=0
+        )
+
+    print(intermediate_embeddings.shape, consolidated_embeddings.shape)
+
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    consolidated_metadata.to_csv(os.path.join(output_dir, "metadata.csv"), index=False)
 
     np.save(
         os.path.join(output_dir, "embeddings.npy"), consolidated_embeddings,
@@ -44,9 +80,20 @@ def postprocess_sentence_embeddings(input_dir, output_dir, key="DHSID_EA"):
 if __name__ == "__main__":
     for country in COUNTRIES:
         for target in TARGETS:
-            if os.path.exists(os.path.join(INTERMEDIATE_DATA_DIR, country, target)):
+            if os.path.exists(
+                os.path.join(INTERMEDIATE_SENTENCE_DATA_DIR, country, target)
+            ):
                 print(country, target)
-                postprocess_sentence_embeddings(
-                    input_dir=os.path.join(INTERMEDIATE_DATA_DIR, country, target),
-                    output_dir=os.path.join(OUTPUT_DATA_DIR, country, target),
+                post_process_sentence_embeddings(
+                    input_dir=os.path.join(
+                        INTERMEDIATE_SENTENCE_DATA_DIR, country, target
+                    ),
+                    output_dir=os.path.join(OUTPUT_SENTENCE_DATA_DIR, country, target),
                 )
+
+        if os.path.exists(os.path.join(OUTPUT_SENTENCE_DATA_DIR, country)):
+            print(country, "all")
+            post_process_sentence_embeddings_all(
+                input_dir=os.path.join(OUTPUT_SENTENCE_DATA_DIR, country),
+                output_dir=os.path.join(OUTPUT_SENTENCE_DATA_DIR, country, "all"),
+            )
