4853152fb738e1453baf56d04e9c68076f13db3f
AndrewJGaut <ajg@umail.ucsb.edu>
2021-11-30 19:04:39 -0800

    added in the experiment framework

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -15,6 +15,7 @@ from typing import List
 
 from enum import Enum
 
+import traceback
 from file_utils import *
 
 
@@ -51,26 +52,39 @@ class ModelInterface:
     def predict(self, X):
         pass
 
-
+class MetricFunction:
+    """
+    Similar to ModelInterface, this defines what functions should be supported by metrics which are passed in
+    to the Experiment class.
+    I'll define this here: this really should just be a function which takes in two parameters: the true test labels
+    and the test predictions (in that order!)
+    """
 
 
 
 class Experiment:
 
     def __init__(self, classification_cutoff_dict: dict, targets: List[str], feature_types: List[FeatureType],
-                 model_types: List[ModelType], models: List[object]):
+                 model_types: List[ModelType], models: List[ModelInterface], metrics: dict[ModelType, List[MetricFunction]]):
         """
 
         :param classification_cutoff_dict:
+
         :param targets: what labels to predict on
+
         :param feature_types: List of features to use when loading dataset.
         All elements should be a value in ['target_sentence', 'all_sentence', 'document', 'target_all_sentence', 'target_sentence_document', 'all_sentence_document']:
+
         :param model_types: Whether models are classification or regression.
         This list should have the same size as the list of models!
         model_types[i] should be the type of model (Regression or Classification) corresponding to models[i]
+
         :param models: The list of model objects.
         These should all support the methods given in ModelInterface so that the Experiment class's functions
         will work for all models in the list.
+
+        :param metrics: a mapping from model type to the metrics to be used for that model type.
+        Note taht we use a mapping here because the metrics used for classification and regression should be different.
         """
         assert len(model_types) == len(models), "model_types list must have same length as models list"
         self.classification_cutoff_dict = classification_cutoff_dict
@@ -78,6 +92,7 @@ class Experiment:
         self.feature_types = feature_types
         self.model_types = model_types
         self.models = models
+        self.metrics = metrics
 
     def get_model_name(self, model: object) -> str:
         """
@@ -87,17 +102,21 @@ class Experiment:
         return type(model).__name__
 
 
-    def get_experiment_name(self, model: object, model_type: ModelType) -> str:
+    def get_experiment_name(self, model: object, model_type: ModelType,
+                            feature_type: FeatureType, target: str) -> str:
         """
         :param model:
         :param model_type:
+        :param feature_type:
+        :param target: These four parameters are just the current values of each of these for a single running
+        experiment. (See this functions usage in the run_experiments functin)
         :return: A string representing the name of this experiment.
          (This is so that we can use this name for save files so that we'll know which results pertain to which
          experiment.)
         """
-        feature_types_string = "-".join(self.feature_types)
-        targets_string = "-".join(self.targets)
-        return f"{self.get_model_name(model)}_{model_type.name}_{feature_types_string}_{targets_string}"
+
+        """NOTE: we may want to add metrics in here."""
+        return f"{self.get_model_name(model)}_{model_type.name}_{feature_type.name}_{target}"
 
     def run_experiments(self):
         """
@@ -107,23 +126,36 @@ class Experiment:
         for model,model_type in zip(self.models, self.model_types):
             for target in self.targets:
                 for feature_type in self.feature_types:
-                    model = self.models[i]
+                    experiment_name = self.get_experiment_name(model, model_type, target, feature_type)
+                    try:
+                        ds = SustainBenchTextDataset(
+                            data_dir=PATH_TO_DATA_DIR,
+                            feature_type=feature_type.name,
+                            target=target,
+                            model_type=model_type.name,
+                            classification_cutoff=self.classification_cutoff_dict[target]
+                        )
 
-                    ds = SustainBenchTextDataset(
-                        data_dir=PATH_TO_DATA_DIR,
-                        feature_type=feature_type,
-                        target=target,
-                        model_type=model_type,
-                        classification_cutoff=self.classification_cutoff_dict[target]
-                    )
+                        train_X, train_y = ds.get_data('train')
+                        test_X, test_y = ds.get_data('test')
 
-                    train_X, train_y = ds.get_data('train')
-                    test_X, test_y = ds.get_data('test')
+                        start = time.time()
+                        model.fit(train_X, train_y)
+                        end = time.time()
+                        print(f"Fit Time for experiment {experiment_name}: {end - start}")
 
-                    start = time.time()
-                    clf.fit(train_embeddings, train_labels)
-                    end = time.time()
-                    print(f"Fit Time for {self.get_model_name(i)} with target {target} and feature_type {feature_type}: {end - start}")
+                        predicted_test_y = model.predict(test_X)
+
+                        results = dict()
+                        for metric in self.metrics[model_type]:
+                            results[metric.__name__] = metric(test_y, predicted_test_y)
+
+                        writeToJsonFile(os.path.join(PATH_TO_RESULTS_DIRECTORY, f"{experiment_name}.json"))
+                    except Exception as e:
+                        print(f"Got exception {e} for experiment {experiment_name}.\nFull stack trace: {traceback.print_exc()}")
+
+
+
 
 
 
